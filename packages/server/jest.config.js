3095193d26e382758430ea08408574fd53abe768
jhony-24 <jhonyvegacuya24@gmail.com>
2021-06-29 17:39:58 -0500

    chore(server): add linter and module alias

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFilesAfterEnv: ["./src/setupTest.ts"],
+  testRegex: ["(/__tests__/.*|(\\.|/)(test|spec))\\.(js?|ts?)"],
 };
