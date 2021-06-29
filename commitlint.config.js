8f0f5b459e4ca8461ef7d096844b4658150e1feb
jhony-24 <jhonyvegacuya24@gmail.com>
2021-06-29 10:48:33 -0500

    chore: add linters

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+/**
+ * @type import("@commitlint/types").UserConfig
+ */
+ module.exports = {
+    extends: "@commitlint/config-conventional",
+    rules: {
+      "subject-min-length" : [
+        2,
+        "always",
+      ],
+      "scope-enum": [
+        2,
+        "always",
+        [
+          "client",
+          "server",
+          "types",
+          "design-system",
+        ],
+      ],
+    },
+  };
