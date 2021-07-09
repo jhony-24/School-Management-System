0829c853092b7f494e73a512bbecf42a44ddbc16
BiloCode <billy_paredes_aycho@hotmail.com>
2021-07-08 21:41:01 -0500

    feat(server): firebase admin configuration

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -10,6 +10,7 @@ import express, { json, urlencoded } from 'express';
 import apiConfig from '@api';
 import Logger from '@utils/Logger';
 import typeORM from '@config/typeORM';
+import firebaseAdmin from '@config/firebase';
 
 dotenv.config();
 
@@ -17,14 +18,15 @@ export async function server() {
   const app = express();
   const serverPort = process.env.PORT || 7000;
 
+  await typeORM();
+  await firebaseAdmin();
+
   app.use(json());
   app.use(urlencoded({ extended: true }));
   app.use(cors());
   app.use(helmet());
   app.use(compression());
   app.use(apiConfig());
-
-  await typeORM();
 
   app.listen(serverPort, () => {
     Logger.info(serverPort as string);
