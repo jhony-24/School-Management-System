70f0f779118332513a0d5ba699f2971332f96b20
jhony-24 <jhonyvegacuya24@gmail.com>
2021-07-09 22:10:42 -0500

    refactor(server): try errors in firebase config

--- packages/server/src/config/firebase.ts
+++ packages/server/src/config/firebase.ts
@@ -1,17 +1,22 @@
 import fireorm from 'fireorm';
 import admin from 'firebase-admin';
-
-import firebaseSecretKey from 'firebaseSecretKey.json';
+import Logger from '@utils/Logger';
+import firebaseSecretKey from './firebaseSecretKey.json';
 
 const firebaseInitialize = async () => {
   if (admin.apps.length) return;
 
-  admin.initializeApp({
-    credential: admin.credential.cert(firebaseSecretKey),
-  });
-
-  const firestore = admin.firestore();
-  fireorm.initialize(firestore);
+  try {
+    admin.initializeApp({
+      credential: admin.credential.cert(firebaseSecretKey),
+    });
+  
+    const firestore = admin.firestore();
+    fireorm.initialize(firestore);
+  }
+  catch (error) {
+    Logger.error(error.message);
+  }
 };
 
 export default firebaseInitialize;
