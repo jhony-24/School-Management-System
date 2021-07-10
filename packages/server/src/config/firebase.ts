22d0c419a9b9c15ece25b235fc2243e7075278e5
jhony-24 <jhonyvegacuya24@gmail.com>
2021-07-10 14:13:38 -0500

    fix(server): parse firebase types of secret keys

--- packages/server/src/config/firebase.ts
+++ packages/server/src/config/firebase.ts
@@ -3,12 +3,14 @@ import admin from 'firebase-admin';
 import Logger from '@utils/Logger';
 import firebaseSecretKey from './firebaseSecretKey.json';
 
+const serviceAccount = firebaseSecretKey as admin.ServiceAccount;
+
 const firebaseInitialize = async () => {
   if (admin.apps.length) return;
 
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(firebaseSecretKey),
+      credential: admin.credential.cert(serviceAccount),
     });
   
     const firestore = admin.firestore();
