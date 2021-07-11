f9b10a42f3830fc22bbb5574ab9559f8db475b54
BiloCode <billy_paredes_aycho@hotmail.com>
2021-07-10 22:34:12 -0500

    feat(server): token decode added

--- packages/server/src/__tests__/controllers/testController.test.ts
+++
@@ -1,9 +0,0 @@
-import testController from '@controllers/testController';
-
-describe('test controller', () => {
-  test('should get a text', () => {
-    const informationController = testController();
-
-    expect(informationController).not.toBe('');
-  });
-});
