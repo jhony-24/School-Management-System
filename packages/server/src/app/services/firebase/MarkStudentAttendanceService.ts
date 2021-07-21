23d8649e6aeeddba30bbc7aa5a70af2dd72bb614
jhony-24 <jhonyvegacuya24@gmail.com>
2021-07-20 20:04:45 -0500

    fix(server): use correctly the prop transaction

--- packages/server/src/app/services/firebase/MarkStudentAttendanceService.ts
+++ packages/server/src/app/services/firebase/MarkStudentAttendanceService.ts
@@ -8,41 +8,46 @@ import { getRepository } from 'fireorm';
 
 export default class MarkStudentAttendanceService implements IMarkStudentAttendance {
   async mark(payload: MarkStudentAttendancePayload): Promise<boolean> {
-    const { studentId, classroomId, date } = payload;
+    const { studentId, classroomId, date, reason } = payload;
     const classroomRepository = getRepository(ClassroomCalendar);
 
-    const response = await classroomRepository.runTransaction(async () => {
-      try {
-        const classroomCalendar = await classroomRepository.whereEqualTo('classroomId', classroomId).findOne();
-        if (!classroomCalendar) {
-          throw new Error('Clase no encontrada');
-        }
-        const studentAttendance = new StudentAttendance(classroomCalendar);
-        const currentAssistanceIndex = studentAttendance.getCurrentIndexAssistanceByDate(date);
-        const assistance = studentAttendance.getCurrentAssistance();
-        const student = studentAttendance.getStudentFromAssistance(studentId, assistance);
-
-        if (student) {
-          const newStudents = studentAttendance.removeStudentFromAssistance(student, assistance);
-          classroomCalendar.assistance[currentAssistanceIndex].students =
-            newStudents;
-        } else {
-          const newStudent = {
-            studentId: payload.studentId,
-            reason: payload.reason,
-          };
-          classroomCalendar.assistance[currentAssistanceIndex].students.push(
-            newStudent
+    const response = await classroomRepository.runTransaction( async (transaction) => {
+        try {
+          const classroomCalendar = await transaction.whereEqualTo('classroomId', classroomId).findOne();
+          if (!classroomCalendar) {
+            throw new Error('Clase no encontrada');
+          }
+          const studentAttendance = new StudentAttendance(classroomCalendar);
+          const currentAssistanceIndex =
+            studentAttendance.getCurrentIndexAssistanceByDate(date);
+          const assistance = studentAttendance.getCurrentAssistance();
+          const student = studentAttendance.getStudentFromAssistance(
+            studentId,
+            assistance
           );
-        }
 
-        await classroomRepository.update(classroomCalendar);
-        return true;
-      } catch (error) {
-        throw new Error(error.message);
-      }
+          if (student) {
+            const newStudents = studentAttendance.removeStudentFromAssistance(
+              student,
+              assistance
+            );
+            classroomCalendar.assistance[currentAssistanceIndex].students = newStudents;
+          } else {
+            const newStudent = {
+              studentId,
+              reason,
+            };
+            classroomCalendar.assistance[currentAssistanceIndex].students.push(
+              newStudent
+            );
+          }
+
+          await transaction.update(classroomCalendar);
+          return true;
+        } catch (error) {
+          throw new Error(error.message);
+        }
     });
     return response;
-
   }
 }
