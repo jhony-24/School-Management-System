16e924875c224ab723679a0d74adf32c0b797b62
BiloCode <billy_paredes_aycho@hotmail.com>
2021-07-24 16:35:22 -0500

    fix: install packages in all folders fixed

--- packages/server/src/app/interfaces/IGetClassroomCalendar.ts
+++
@@ -1,9 +0,0 @@
-import { IClassroom, IClassroomCalendar } from '@system/types';
-
-export interface IGetClassroomPayload extends Partial<IClassroom> {}
-
-export interface IGetClassroomCalendar {
-  getClassroomCalendar(
-    classroom: IGetClassroomPayload
-  ): Promise<IClassroomCalendar | null>;
-}
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -9,6 +9,26 @@ import RelationshipType from './src/RelationshipType';
 import SchoolGradeType from './src/SchoolGradeType';
 import UserType from './src/UserType';
 
+// MYSQL TYPES
+import IStudentTutors from './src/models/mysql/IStudentTutors';
+import IClassroomStudent from './src/models/mysql/IClassroomStudent';
+import IDirector from './src/models/mysql/IDirector';
+import IHomework from './src/models/mysql/IHomework';
+import IStudent from './src/models/mysql/IStudent';
+import ITeacher from './src/models/mysql/ITeacher';
+import ITutor from './src/models/mysql/ITutor';
+import IUser from './src/models/mysql/IUser';
+
+// FIREBASE TYPES
+import IClassroom from './src/models/firebase/IClassroom';
+import IClassroomCalendar, {
+  IAssistance,
+} from './src/models/firebase/IClassroomCalendar';
+import IFileSection from './src/models/firebase/IFileSection';
+import IPublications from './src/models/firebase/IPublications';
+import ISchool from './src/models/firebase/ISchool';
+import ISystemStorage from './src/models/firebase/ISystemStorage';
+
 export {
   DateRangeType,
   FilePermissionType,
@@ -22,18 +42,7 @@ export {
   UserType,
 };
 
-//MYSQL TYPES
-import IClassroomStudentPerYear from './src/models/mysql/IClassroomStudentPerYear';
-import IStudentTutors from './src/models/mysql/IStudentTutors';
-import IDirector from './src/models/mysql/IDirector';
-import IHomework from './src/models/mysql/IHomework';
-import IStudent from './src/models/mysql/IStudent';
-import ITeacher from './src/models/mysql/ITeacher';
-import ITutor from './src/models/mysql/ITutor';
-import IUser from './src/models/mysql/IUser';
-
 export {
-  IClassroomStudentPerYear,
   IDirector,
   IHomework,
   IStudent,
@@ -43,14 +52,6 @@ export {
   IStudentTutors,
 };
 
-//FIREBASE TYPES
-import IClassroom from './src/models/firebase/IClassroom';
-import IClassroomCalendar, { IAssistance } from './src/models/firebase/IClassroomCalendar';
-import IFileSection from './src/models/firebase/IFileSection';
-import IPublications from './src/models/firebase/IPublications';
-import ISchool from './src/models/firebase/ISchool';
-import ISystemStorage from './src/models/firebase/ISystemStorage';
-
 export {
   IClassroom,
   IAssistance,
@@ -59,4 +60,5 @@ export {
   IFileSection,
   IClassroomCalendar,
   ISystemStorage,
+  IClassroomStudent,
 };
