8232e25988a8bcb6fcb530d022b9fe0144e174ee
BiloCode <billy_paredes_aycho@hotmail.com>
2021-08-10 10:12:36 -0500

    fix(web_application): fix version in babel core

--- packages/web_application/App.tsx
+++ packages/web_application/App.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import RouterApplication from './src/router';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar />
       <RouterApplication />
-    </View>
+    </SafeAreaView>
   );
 }
 
---
+++ packages/web_application/src/config/colors.ts
@@ -0,0 +1,12 @@
+enum Colors {
+  BLUE = '#0D54DD',
+  WHITE = '#FFF',
+  WHITE_SMOOTH = 'rgba(255,255,255,.16)',
+  RED = '#FF1B51',
+  GREEN = '#00FF85',
+  BLACK = '#2F2F2F',
+  BLACK_SMOOTH = 'rgba(0,0,0, .6)',
+  GRAY = '#878787',
+}
+
+export default Colors;
