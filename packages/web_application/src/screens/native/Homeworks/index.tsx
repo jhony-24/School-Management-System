efe247da5a4b9f340d6c69fbe6459fb722039983
jhony-24 <jhonyvegacuya24@gmail.com>
2021-08-29 20:30:09 -0500

    feat(web_application): create basic homeworks screen

--- packages/web_application/src/screens/native/Homeworks/index.tsx
+++ packages/web_application/src/screens/native/Homeworks/index.tsx
@@ -1,9 +1,21 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
+import Text from '../../../components/atoms/Text';
+import { Fonts } from '../../../styles/font';
+import { HomeworkItemDivider, ListHomeworks } from './style';
+import HomeworkItem from '../../../components/molecules/HomeworkItem';
+import { HomeworkState } from '../../../styles/homework';
+import SearchBar from '../../../components/molecules/SearchBar';
 
 const Homeworks = () => (
   <View>
-    <Text>Homeworks</Text>
+    <SearchBar />
+    <Text weight={Fonts.BOLD}>21 resultados</Text>
+    <ListHomeworks>
+      <HomeworkItemDivider>
+        <HomeworkItem course="Matematica" state={HomeworkState.ok} date="now" />
+      </HomeworkItemDivider>
+    </ListHomeworks>
   </View>
 );
 
---
+++ packages/web_application/src/screens/native/Homeworks/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import { Colors } from '../../../styles/colors';
+
+export const ListHomeworks = styled.View``;
+
+export const HomeworkItemDivider = styled.View`
+  border-bottom: 1px solid ${Colors.WHITE_LIGHT};
+`;
